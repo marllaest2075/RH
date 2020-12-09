fc154954235ad05c5d2aca145aaa91c48de613c7
Marllaest2075 <Marllaest2075@gmail.com>
2020-12-08 18:31:23 -0700

    Creacion

---
+++ Src/Controles/CustomTabButton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controles
+{
+   
+    public class CustomTabButton : Button
+    {
+        static CustomTabButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomTabButton), new FrameworkPropertyMetadata(typeof(CustomTabButton)));
+        }
+
+        private Image image;
+
+        public Image Imagen
+        {
+            get { return image; }
+            set { image = value; }
+        }
+
+
+    }
+}
